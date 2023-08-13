07d69f4174e42da683da0ba943e836450400c097
Sevi Gouland <sevigouland@gmail.com>
2023-08-14 00:47:30 +0300

    add google fonts, fig bg img

--- home-page.js
+++ home-page.js
@@ -1,9 +1,7 @@
 function myFunction() {
     document.getElementsByClassName("btn1");
-    alert("hello");
 }
 
 function myFunction2() {
     document.getElementsByClassName("btn2");
-    alert("kalispera");
 }
