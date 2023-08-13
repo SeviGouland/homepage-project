8c68e5745b39773e939c12987a694fbda7e3c905
Sevi Gouland <sevigouland@gmail.com>
2023-08-13 11:21:22 +0300

    add button functionality, refactor html and css

---
+++ home-page.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementsByClassName("btn1");
+    alert("hello");
+}
+
+function myFunction2() {
+    document.getElementsByClassName("btn2");
+    alert("kalispera");
+}
