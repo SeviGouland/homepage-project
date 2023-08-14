f75db5b260d18aef18f49a8a366a53a8d82e2b90
Sevi Gouland <sevigouland@gmail.com>
2023-08-14 14:41:54 +0300

    add menu

--- home-page.js
+++ home-page.js
@@ -1,3 +1,7 @@
+// window.onload= function(){
+//     showMarmariDetails();
+//     }
+
 function myFunction() {
     document.getElementsByClassName("btn1");
 }
@@ -5,3 +9,13 @@ function myFunction() {
 function myFunction2() {
     document.getElementsByClassName("btn2");
 }
+
+
+
+function showMarmariDetails() {
+   let menu1 = document.getElementsByClassName("menu-btn-1");
+   menu1[0].style.display = "inline";
+} 
+
+
+
